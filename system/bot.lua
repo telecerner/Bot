da1d54ceef3cccffebb179d7785808225dfafdf7
telecerner <amircerner@gmail.com>
2016-11-25 14:02:34 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,18 +234,10 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {299362763,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
-This is an original bot and based on (AdvanSource, version 4 final).
-Copyright all right reserved and you must respect all laws.
-
-Source: https://github.com/janlou/AdvanSource
-Channel: @AdvanTm
-Messenger: @Advanbot
-Creator: @janlou
-Site: http://StoreVps.ir
 ]],
     --Start rate:
     Group_rate = [[]],
