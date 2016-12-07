c5f2e090dce96d0b67f43344e963a2d862057110
telecerner <amircerner@gmail.com>
2016-12-07 22:00:06 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -234,11 +234,10 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {299362763,tonumber(our_id)},--Sudo users
+    sudo_users = {261910011,285663053,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
-    about_text = [[*IN THE NAME OF ALLAH*
-]],
+    about_text = [[]],
     --Start rate:
     Group_rate = [[]],
     Supergroup_rate = [[]]
